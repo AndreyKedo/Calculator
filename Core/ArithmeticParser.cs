722b338044bc88ab908091f55f60c17e6c989fe8
Andrey Kedo <korvodre2015@yandex.ru>
2018-07-22 19:48:44 +0700

    Добавьте файлы проекта.

---
+++ Core/ArithmeticParser.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public static class ArithmeticParser
+    {
+        private static string Str { get; set; }
+        private static Func<char, uint> countS = (ch) =>
+        {
+            uint _count = 0;
+            for (int i = 0; i < Str.Length; i++)
+                if (Str[i] == ch)
+                {
+                    _count++;
+                }
+            return _count;
+        };
+
+        public static string ToParse(string str)
+        {
+            double outpute = 0;
+            if (str != "")
+            {
+                Str = "(" + str + ")";
+                if (IfBracketsEqual())
+                {
+                    outpute = ExpressionHandling(Str);
+                    if (Double.IsInfinity(outpute))
+                        return "Деление на ноль невозможно";
+
+                    return outpute.ToString();
+                }
+            }
+            return "Empty line";
+        }
+
+        private static double ExpressionHandling(string str)
+        {
+            int i = 0;
+            char chr;
+            Stack<double> number = new Stack<double>();
+            Stack<char> operation = new Stack<char>();
+            while (i < str.Length)
+            {
+                chr = str[i];
+                if (Char.IsDigit(chr))
+                {
+                    number.Push(Double.Parse(NumberConcatination(ref i, str)));
+                }
+                else if (operation.Count == 0 || chr == '(')
+                {
+                    operation.Push(chr);
+                }
+                else if (chr == '+' || chr == '-' || chr == '*' || chr == '/')
+                {
+                    if (PriorityOperation(chr) > PriorityOperation(operation.Peek()))
+                    {
+                        operation.Push(chr);
+                    }
+                    else if (PriorityOperation(chr) == PriorityOperation(operation.Peek()))
+                    {
+                        while (operation.Count != 0 && (PriorityOperation(chr) == PriorityOperation(operation.Peek())))
+                        {
+                            Calc(operation.Pop(), ref number);
+                        }
+                        if (operation.Count != 0 && (PriorityOperation(chr) > PriorityOperation(operation.Peek())))
+                        {
+                            operation.Push(chr);
+                        }
+                    }
+                    else
+                    {
+                        while (PriorityOperation(chr) < PriorityOperation(operation.Peek()))
+                        {
+                            Calc(operation.Pop(), ref number);
+                        }
+                        operation.Push(chr);
+                    }
+                }
+                else
+                {
+                    while (operation.Peek() != '(')
+                    {
+                        Calc(operation.Pop(), ref number);
+                    }
+                    operation.Pop();
+                }
+                if (i < str.Length)
+                    i++;
+            }
+            return number.Pop();
+        }
+
+        private static string NumberConcatination(ref int index, string str)
+        {
+            string buffer = String.Empty;
+            int j = index;
+            while (Char.IsDigit(str[j]))
+            {
+                buffer += str[j];
+                j++;
+            }
+            index = (j - 1);
+            return buffer;
+        }
+
+        private static string FractionalNumber(string[] str)
+        {
+            string _joinStr = "";
+            _joinStr = string.Join(_joinStr, str, 0, str.Length);
+            return _joinStr;
+        }
+
+        private static void Calc(char chr, ref Stack<double> num)
+        {
+            double buff = 0;
+            switch (chr)
+            {
+                case '+':
+                    buff = num.Pop();
+                    num.Push(num.Pop() + buff);
+                    break;
+                case '-':
+                    buff = num.Pop();
+                    num.Push(num.Pop() - buff);
+                    break;
+                case '*':
+                    buff = num.Pop();
+                    num.Push(num.Pop() * buff);
+                    break;
+                case '/':
+                    buff = num.Pop();
+                    num.Push(num.Pop() / buff);
+                    break;
+            }
+        }
+
+        private static byte PriorityOperation(char chr)
+        {
+            switch (chr)
+            {
+                case '+':
+                    return 1;
+                case '-':
+                    return 2;
+                case '*':
+                case '/':
+                    return 3;
+            }
+            return 0;
+        }
+
+        private static bool IfBracketsEqual()
+        {
+            return (countS('(') == countS(')'));
+        }
+
+    }
+}
---
+++ MathAppCalculator/ViewModel/ViewModel.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Linq;
+
+namespace MathAppCalculator.ViewModel
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private const string _ExpressionText = "Выражение";
+        private string _Expression = _ExpressionText;
+        public string Expression
+        {
+            get
+            {
+                return _Expression;
+            }
+            set
+            {
+                _Expression = value;
+                OnPropertyChanged(nameof(Expression));
+            }
+        }
+
+        #region Нумирация
+        public ICommand Number
+        {
+            get
+            {
+                return new DelegateCommand((str) =>
+                {
+                    if (Expression == _ExpressionText) { Expression = ""; }
+                    Expression += str.ToString();
+                });
+            }
+        }
+        #endregion
+
+        #region Операции
+        public ICommand Operation
+        {
+            get
+            {
+                return new DelegateCommand((str) => 
+                {
+                    if((Expression != _ExpressionText && (Expression.Last() >= '0' && Expression.Last() <= '9')) || Expression.Last() == ')')
+                        Expression += str.ToString();
+                });
+            }
+        }
+        #endregion
+
+        #region Остальные действия
+
+        public ICommand Calc
+        {
+            get
+            {
+                return new DelegateCommand((obj) => 
+                {
+                    if(Expression != _ExpressionText)
+                        Expression = Core.ArithmeticParser.ToParse(Expression);
+                });
+            }
+        }
+
+        public ICommand LeftBrackets
+        {
+            get
+            {
+                return new DelegateCommand((obj) => 
+                {
+                    if (!(Expression.Last() >= '0' && Expression.Last() <= '9'))
+                    {
+                        if (Expression == _ExpressionText)
+                            Expression = "";
+                        Expression += "(";
+                    }
+                });
+            }
+        }
+
+        public ICommand RightBrackets
+        {
+            get
+            {
+                return new DelegateCommand((obj) =>
+                {
+                    if (Expression != _ExpressionText && (Expression.Last() >= '0' && Expression.Last() <= '9'))
+                    {
+                        Expression += ")";
+                    }
+                });
+            }
+        }
+
+        public ICommand Negative
+        {
+            get
+            {
+                return new DelegateCommand((obj) => 
+                {
+                    if ((Expression.Last() == '(' || Expression.Last() == '*' || Expression.Last() == '/') || Expression == _ExpressionText) {
+                        if (Expression == "Выражение")
+                            Expression = "";
+                        Expression += "-";
+                    }
+                });
+            }
+        }
+
+        public ICommand Dot
+        {
+            get
+            {
+                return new DelegateCommand((obj) => 
+                {
+                    if (Expression.Last() >= '0' && Expression.Last() <= '9')
+                        Expression += ".";
+                });
+            }
+        }
+
+        public ICommand Clean
+        {
+            get
+            {
+                return new DelegateCommand((obj) =>
+                {
+                    Expression = _ExpressionText;
+                });
+            }
+        }
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string str)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(str));
+        }
+    }
+
+    class DelegateCommand : ICommand
+    {
+        private Action<object> action;
+        private Func<object, bool> func;
+
+        public DelegateCommand(Action<object> actionExecute, Func<object, bool> funcExecute)
+        {
+            action = actionExecute;
+            func = funcExecute;
+        }
+
+        public DelegateCommand(Action<object> actionExecute) : this(actionExecute, null)
+        {
+            action = actionExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return func != null ? func.Invoke(parameter) : true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action?.Invoke(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+
+}
